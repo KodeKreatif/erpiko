14a1438c2530f62c6cc1869b60c544978f1d1f84
Mohammad Anwari <mdamt@mdamt.net>
2016-04-24 12:49:43 +0700

    Import/export to/from DER/PEM

--- include/key.h
+++ include/key.h
@@ -2,6 +2,7 @@
 #define _KEY_H_
 
 #include <memory>
+#include <vector>
 
 namespace Erpiko {
 
@@ -13,13 +14,51 @@ class RsaKey {
     RsaKey();
     virtual ~RsaKey();
 
+    /**
+     * Creates a new RsaKey pair
+     * @param bits number of bits
+     * @return a new instance of RsaKey initialized to the number of bits as specified
+     */
     static RsaKey* create(const unsigned int bits);
 
     /**
+     * Imports a pair of RsaKey from PEM
+     * @param pem string containing PEM data
+     * @param passphrase the passphrase to import the PEM
+     * @return a new instance of RsaKey
+     */
+    static RsaKey* fromPem(const std::string pem, const std::string passphrase = "");
+
+    /**
+     * Imports a pair of RsaKey from DER
+     * @param der vector containing DER data
+     * @param passphrase the passphrase to import the DER
+     * @return a new instance of RsaKey
+     */
+    static RsaKey* fromDer(const std::vector<unsigned char> der, const std::string passphrase = "");
+
+
+    /**
      * Gets the number of bits of this key
+     * @return number of bits
      */
     unsigned int bits() const;
 
+    /**
+     * Gets the PKCS#8 format as PEM string (if passphrase is given), otherwise
+     * returns a traditional RSA format in encoded PEM
+     * @param passphrase
+     * @return string containing PEM. Empty if something is broken.
+     */
+    const std::string toPem(const std::string passphrase = "") const;
+
+    /**
+     * Gets the PKCS#8 format as PEM string (if passphrase is given), otherwise
+     * returns a traditional RSA format in encoded PEM
+     * @param passphrase
+     * @return string containing PEM. Empty if something is broken.
+     */
+    const std::vector<unsigned char> toDer(const std::string passphrase = "") const;
 
   private:
     class Impl;
--- tests/key.cpp
+++ tests/key.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 
+#include "utils.h"
 #include "key.h"
 
 namespace Erpiko {
@@ -21,6 +22,67 @@ SCENARIO("Keys can be created") {
     RsaKey* pair = RsaKey::create(1024);
     THEN("Key is correctly initialized") {
       REQUIRE(pair->bits() == 1024);
+      THEN("Key is exported as PEM without password") {
+        auto pem = pair->toPem();
+
+        THEN("Key is able to be reimported") {
+          RsaKey* import = RsaKey::fromPem(pem);
+          REQUIRE(pem == import->toPem());
+        }
+      }
+    }
+  }
+
+  GIVEN("A newly created key pair") {
+    RsaKey* pair = RsaKey::create(1024);
+    THEN("Key is correctly initialized") {
+      REQUIRE(pair->bits() == 1024);
+      THEN("Key is exported as PEM with password") {
+        auto pem = pair->toPem("omama");
+        THEN("Key is able to be reimported") {
+          RsaKey* import = RsaKey::fromPem(pem, "omama");
+          REQUIRE(import->bits() == 1024);
+        }
+        THEN("Key is not importable") {
+          RsaKey* import = RsaKey::fromPem(pem, "wrong");
+          REQUIRE(import == nullptr);
+        }
+      }
+    }
+  }
+
+  GIVEN("A newly created key pair") {
+    RsaKey* pair = RsaKey::create(1024);
+    THEN("Key is correctly initialized") {
+      REQUIRE(pair->bits() == 1024);
+      THEN("Key is exported as DER without password") {
+        auto der = pair->toDer();
+        THEN("Key is able to be reimported") {
+          RsaKey* import = RsaKey::fromDer(der);
+          REQUIRE(der == import->toDer());
+          REQUIRE(import->bits() == 1024);
+        }
+      }
+    }
+  }
+
+  GIVEN("A newly created key pair") {
+    RsaKey* pair = RsaKey::create(1024);
+    THEN("Key is correctly initialized") {
+      REQUIRE(pair->bits() == 1024);
+      THEN("Key is exported as DER with password") {
+        auto der = pair->toDer("omama");
+        THEN("Key is able to be reimported") {
+          RsaKey* import = RsaKey::fromDer(der, "omama");
+          REQUIRE_FALSE(import == nullptr);
+          REQUIRE(import->bits() == 1024);
+        }
+
+        THEN("Key is not importable with wrong password") {
+          RsaKey* import = RsaKey::fromDer(der, "wrong");
+          REQUIRE(import == nullptr);
+        }
+      }
     }
   }
 
