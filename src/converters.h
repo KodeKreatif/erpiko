ba00f6da603665d751395b9d7544e5ad7de91d5c
Mohammad Anwari <mdamt@mdamt.net>
2016-05-03 22:38:20 +0700

    certificateToDer function

--- src/converters.h
+++ src/converters.h
@@ -4,14 +4,17 @@
 #include "erpiko/identity.h"
 #include "erpiko/bigint.h"
 
+#include <openssl/err.h>
 #include <openssl/pem.h>
 #include <openssl/bio.h>
 #include <openssl/evp.h>
 #include <openssl/bn.h>
 #include <openssl/rsa.h>
 #include <openssl/x509.h>
+#include <openssl/pkcs12.h>
 
 #include <memory>
+#include <iostream>
 
 namespace Erpiko {
 namespace Converters {
@@ -81,6 +84,50 @@ namespace Converters {
 
     return retval;
   }
+
+  inline std::vector<unsigned char> certificateToDer(X509* r) {
+    std::vector<unsigned char> v;
+    int length = i2d_X509(r, 0);
+    if (length) {
+      unsigned char *der = (unsigned char*)malloc(length);
+      // openssl will advances this pointer after populating
+      unsigned char *start = der;
+      i2d_X509(r, &der);
+      for (int i = 0; i < length; i ++) {
+        v.push_back(start[i]);
+      }
+      free(start);
+    }
+
+    return v;
+  }
+
+  inline std::vector<unsigned char> pkcs12ToDer(PKCS12* r) {
+    std::vector<unsigned char> retval;
+    int ret;
+    BIO* mem = BIO_new(BIO_s_mem());
+
+    ret = i2d_PKCS12_bio(mem, r);
+    std::cout << ret << "\n";
+
+        ERR_print_errors_fp (stderr);
+
+    while (ret) {
+      unsigned char buff[1024];
+      int ret = BIO_read(mem, buff, 1024);
+      if (ret > 0) {
+        for (int i = 0; i < ret; i ++) {
+          retval.push_back(buff[i]);
+        }
+      } else {
+        break;
+      }
+    }
+    BIO_free(mem);
+
+    return retval;
+  }
+
 
 
 
